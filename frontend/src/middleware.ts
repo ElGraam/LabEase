620951841053cc5f413ab338ae2356e69ec18c00
ElGraam <178278835+ElGraam@users.noreply.github.com>
2024-12-11 01:59:48 +0900

    Add layout components and middleware for session management

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,14 @@
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(req: NextRequest) {
+  // セッションのトークンを取得
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  // リクエストのパスを取得
+  const { pathname } = req.nextUrl;
+
+
+  // 通常のレスポンスを返す
+  return NextResponse.next();
+}
