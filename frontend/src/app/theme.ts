19030732b3d83485e6633c750a2c098eb75dbf63
ElGraam <178278835+ElGraam@users.noreply.github.com>
2024-12-11 02:00:24 +0900

    Add custom theme and update Home component layout with Chakra UI

---
+++ frontend/src/app/theme.ts
@@ -0,0 +1,30 @@
+import { extendTheme } from '@chakra-ui/react';
+
+/** Chakra UIのカスタムテーマ */
+const theme = extendTheme({
+  styles: {
+    global: {
+      'html, body': {
+        color: 'app.text',
+        overflowX: 'clip',
+      },
+    },
+  },
+  colors: {
+    app: {
+      blue: '#3182CE',
+      bg: '#F7FAFC',
+      border: '#E2E8F0',
+      text: '#333',
+      disable: '#A0AEC0',
+    },
+  },
+  fontSizes: {
+    app: {
+      header1: '24px',
+      header2: '20px',
+    },
+  },
+});
+
+export default theme;
