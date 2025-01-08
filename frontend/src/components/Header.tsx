175ee3f2b5830bac649b9dfcedd37aae7499eb6d
ElGraam <178278835+ElGraam@users.noreply.github.com>
2025-01-08 15:59:57 +0900

    Add a hamburger menu to the Header component and adjust the layout of the logo and menu.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,10 @@
 import HeaderActionMenu from "@/components/HeaderActionMenu";
 import { Box, Flex, Image, Link, SystemStyleObject } from "@chakra-ui/react";
 import NextLink from "next/link";
+import HamburgerMenu from "@/components/HamburgerMenu";
 
 const Header = async () => {
+  // 背景のBox
   const bgStyle: SystemStyleObject = {
     position: "sticky",
     top: "0",
@@ -12,12 +14,14 @@ const Header = async () => {
     boxShadow: "base",
   };
 
+  // コンテンツ余白のスタイル
   const boxStyle: SystemStyleObject = {
     maxWidth: "1140px",
     padding: "0 20px",
     margin: "0 auto",
   };
 
+  // ヘッダーコンテントのFlexスタイル
   const contentFlexStyle: SystemStyleObject = {
     backgroundColor: "white",
     width: "100%",
@@ -26,22 +30,49 @@ const Header = async () => {
     justifyContent: "space-between",
   };
 
+  // ロゴ部分のFlexスタイル
+  const logoFlexStyle: SystemStyleObject = {
+    alignItems: "center",
+    gap: "16px", // ロゴとハンバーガーメニューの間のスペースを設定
+    display: "flex",
+  };
+
+  // ヘッダーのロゴ画像のスタイル
   const logoStyle: SystemStyleObject = {
     maxWidth: { base: "221px", md: "unset" },
   };
 
+  // ヘッダーのロゴ画像スタイル
   const logoImageStyle: SystemStyleObject = {
     height: "40px",
+  };
+
+  // メニューを含む右側エリアのFlexスタイル
+  const rightMenuStyle: SystemStyleObject = {
+    alignItems: "center",
+    gap: "20px", // メニュー間の余白
+    display: "flex",
   };
 
   return (
     <Box as="header" sx={bgStyle}>
       <Box sx={boxStyle}>
         <Flex sx={contentFlexStyle}>
-          <Link as={NextLink} href="/" sx={logoStyle}>
-            <Image src="/univ-logo.png" alt="" sx={logoImageStyle} />
-          </Link>
-          <HeaderActionMenu />
+          {/* 左側のロゴとハンバーガーメニュー */}
+          <Flex sx={logoFlexStyle}>
+            {/* ハンバーガーメニュー */}
+            <HamburgerMenu />
+            {/* ロゴ部分 */}
+            <Link as={NextLink} href="/" sx={logoStyle}>
+              <Image src="/univ-logo.png" alt="Logo" sx={logoImageStyle} />
+            </Link>
+          </Flex>
+
+          {/* メニュー部分 */}
+          <Flex sx={rightMenuStyle}>
+            {/* アクションメニュー */}
+            <HeaderActionMenu />
+          </Flex>
         </Flex>
       </Box>
     </Box>
